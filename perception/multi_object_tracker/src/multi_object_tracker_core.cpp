3fbad902cac7483a398c01ee8a28973a83ca2b0a
Lawrence <lawrenceandaugy@gmail.com>
2025-04-07 01:30:01 -0700

    update measurement_count_threshold to be

--- perception/multi_object_tracker/src/multi_object_tracker_core.cpp
+++ perception/multi_object_tracker/src/multi_object_tracker_core.cpp
@@ -317,8 +317,7 @@ void MultiObjectTracker::sanitizeTracker(
 inline bool MultiObjectTracker::shouldTrackerPublish(
   const std::shared_ptr<const Tracker> tracker) const
 {
-  // constexpr int measurement_count_threshold = 3;
-  constexpr int measurement_count_threshold = 5;
+  constexpr int measurement_count_threshold = 6;
   if (tracker->getTotalMeasurementCount() < measurement_count_threshold) {
     return false;
   }
